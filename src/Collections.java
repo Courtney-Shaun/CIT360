756c410d9dab6492b48d14f60f2926f9a5c454c7
Courtney-Shaun <scourtney78@gmail.com>
2020-01-17 22:56:16 -0700

    ArrayList 1

--- src/Collections.java
+++ src/Collections.java
@@ -1,2 +1,30 @@
+import java.util.*;
 public class Collections {
+
+
+        public static void main(String[] args) {
+                System.out.println("Array List");
+
+                List players = new ArrayList();
+                players.add("Carson Wentz");
+                players.add("Miles Sanders");
+                players.add("DeSean Jackson");
+                players.add("Alshon Jeffery");
+                players.add("Lane Johnson");
+                players.add("Brandon Brooks");
+                players.add("Jason Kelce");
+                players.add("Isaac Seumalo");
+                players.add("Jason Peters");
+
+                //Print list using an iterator
+                Iterator iterator = players.iterator();
+
+                System.out.println("Eagles Offense" + "\r");
+                while (iterator.hasNext()) {
+                        System.out.println(iterator.next() + "\r");
+                }
+        }
+
 }
+
+
