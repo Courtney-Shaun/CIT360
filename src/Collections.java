63a0e867c2e6f436764ffe0ae9d9340b56252435
Courtney-Shaun <scourtney78@gmail.com>
2020-01-20 11:26:24 -0700

    Added user input to array list

--- src/Collections.java
+++ src/Collections.java
@@ -1,9 +1,13 @@
 import java.util.*;
+
 public class Collections {
 
 
         public static void main(String[] args) {
                 System.out.println("Array List");
+                Scanner numEntry = new Scanner(System.in);
+                int choice = numEntry.nextInt();
+                System.out.println("Emter a number between 0 and 8");
 
                 List players = new ArrayList();
                 players.add("Carson Wentz");
@@ -16,15 +20,25 @@ public class Collections {
                 players.add("Isaac Seumalo");
                 players.add("Jason Peters");
 
-                //Print list using an iterator
-                Iterator iterator = players.iterator();
-
-                System.out.println("Eagles Offense" + "\r");
-                while (iterator.hasNext()) {
-                        System.out.println(iterator.next() + "\r");
+                Object entry = players.get(choice);
+                if (choice < 9) {
+                        System.out.println("player is " + entry);
+                } else if (choice >= 9){
+                        System.out.println(" number is not valid.");
                 }
+
+
+                //Print list using an iterator
+                //Iterator offense = players.iterator();
+
+                //System.out.println("Eagles Offense" + "\r");
+                //while (offense.hasNext()) {
+                //        System.out.println(offense.next() + "\r");
+                }
+
+                //Set
         }
 
-}
+
 
 
